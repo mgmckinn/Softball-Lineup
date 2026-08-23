3edce564ce9d706040d8d3a5a3ddff18d38d6e23
Mitch McKinney <mgmckinn2325@me.com>
2026-08-23 16:38:50 -0400

    feat: implement versioning and local storage management for data clearing in App component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 /** @format */
 
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import OfflineIndicator from "./components/OfflineIndicator";
@@ -9,7 +9,20 @@ import BattingOrder from "./components/BattingOrder";
 import RotationLog from "./components/RotationLog";
 import "./App.css";
 
+const APP_VERSION = "2.0"; // Updated version for softball positions
+
 function App() {
+  useEffect(() => {
+    // Check if we need to clear old data
+    const storedVersion = localStorage.getItem("appVersion");
+    if (storedVersion !== APP_VERSION) {
+      // Clear old rotation log data when version changes
+      localStorage.removeItem("rotationLog");
+      localStorage.setItem("appVersion", APP_VERSION);
+      console.log("Updated to version", APP_VERSION, "- cleared old data");
+    }
+  }, []);
+
   return (
     <Router
       basename='/Softball-Lineup'
